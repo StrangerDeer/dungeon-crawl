e35d0b2ad906f094e8fcc8f9e47f0b45741370b7
StrangerDeer <113778183+StrangerDeer@users.noreply.github.com>
2023-04-25 12:15:49 +0200

    a skeleton moves back and forth

--- src/main/java/com/codecool/dungeoncrawl/ui/enemiesmove/SkeletonMove.java
+++ src/main/java/com/codecool/dungeoncrawl/ui/enemiesmove/SkeletonMove.java
@@ -1,15 +1,23 @@
 package com.codecool.dungeoncrawl.ui.enemiesmove;
 
+import com.codecool.dungeoncrawl.data.CellType;
 import com.codecool.dungeoncrawl.data.GameMap;
 
 public class SkeletonMove implements EnemiesMoves {
     @Override
     public void perform(GameMap map, int step) {
+        boolean moveRight = true;
+        boolean moveLeft = false;
 
-        if(step <= 5){
+        if(!map.getSkeleton().getCell().getNeighbor(step + 1, 0).getType().equals(CellType.FLOOR)){
+            moveRight = false;
+            moveLeft = true;
+        }
+
+        if(moveRight){
             step++;
             map.getSkeleton().move(step,0);
-        }else{
+        }else if (moveLeft){
             step--;
             map.getSkeleton().move(step, 0);
         }
