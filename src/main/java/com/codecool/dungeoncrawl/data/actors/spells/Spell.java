c91eee19a2b619ca1d3a1e9861427982c717d775
StrangerDeer <113778183+StrangerDeer@users.noreply.github.com>
2023-04-26 19:06:08 +0200

    spell can damage player just right side

--- src/main/java/com/codecool/dungeoncrawl/data/actors/spells/Spell.java
+++ src/main/java/com/codecool/dungeoncrawl/data/actors/spells/Spell.java
@@ -1,6 +1,7 @@
 package com.codecool.dungeoncrawl.data.actors.spells;
 
 import com.codecool.dungeoncrawl.data.Cell;
+import com.codecool.dungeoncrawl.data.CellType;
 import com.codecool.dungeoncrawl.data.actors.Actor;
 import com.codecool.dungeoncrawl.data.actors.enemies.Enemy;
 
@@ -12,35 +13,84 @@ public class Spell extends Enemy {
     private int xCoordinate = 0;
     private int yCoordinate = 0;
 
+    private boolean exist;
+
+    private boolean moveRight;
+    private boolean moveLeft;
+    private boolean moveDown;
+
+    private boolean moveUp;
+
+    private int damage = 1;
+
     public Spell(Cell cell, int x, int y) {
         super(cell);
+
+        if(x > 0){
+            this.moveRight = true;
+            this.exist = true;
+        }else if(x < 0){
+            this.moveLeft = true;
+            this.exist = true;
+        }else if(y > 0){
+            this.moveDown = true;
+            this.exist = true;
+        }else if(y < 0){
+            this.moveUp = true;
+            this.exist = true;
+        }
         this.xCoordinate = x;
         this.yCoordinate = y;
     }
 
+    private void dealPlayerDamage(int position){
+        if(moveRight &&
+                getCell().getNeighbor(position + 1, 0).getActor() != null &&
+                getCell().getNeighbor(position + 1, 0).getActor().getTileName().equals("player") ){
+            getCell().getNeighbor(position + 1, 0).getActor().setHealth(
+                    getCell().getNeighbor(position + 1, 0).getActor().getHealth() - damage);
+            this.exist = false;
+        }
+    }
+
     @Override
     public void moveEnemy() {
+        moveFieldChecker(0);
         makeMove(0);
 
     }
 
     @Override
     protected void moveFieldChecker(int position) {
+        if (moveRight &&
+                getCell().getNeighbor(position + 1, 0).getType().equals(CellType.WALL)){
+            this.exist = false;
+        }else if(moveDown &&
+                getCell().getNeighbor(0, position + 1).getType().equals(CellType.WALL)){
+            this.exist = false;
+        }else if(moveUp &&
+                getCell().getNeighbor(0, position - 1).getType().equals(CellType.WALL)){
+            this.exist = false;
+        }else if(moveLeft &&
+                getCell().getNeighbor(position - 1, 0).getType().equals(CellType.WALL)){
+            this.exist = false;
+        }
 
+        dealPlayerDamage(position);
     }
 
     @Override
     protected void makeMove(int position) {
-        if (xCoordinate > 0){
+        if (moveRight){
             position++;
             move(position,0);
-        }else if(yCoordinate > 0){
+        }else if(moveDown){
             position++;
             move(0,position);
-        }else if(yCoordinate < 0){
+        }else if(moveUp){
             position--;
             move(0,position);
-        }else if(xCoordinate < 0){
+        }else if(moveLeft){
             position--;
             move(position, 0);
         }
@@ -48,7 +98,12 @@ public class Spell extends Enemy {
 
     @Override
     public String getTileName() {
-        return "spell";
+
+        if(exist){
+            return "spell";
+        }
+
+        return "floor";
     }
 
     @Override
