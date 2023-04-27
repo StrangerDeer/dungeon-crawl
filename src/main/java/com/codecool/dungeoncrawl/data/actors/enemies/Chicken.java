3e6e9477d0b34343b648cd1428bebed368433fb3
StrangerDeer <113778183+StrangerDeer@users.noreply.github.com>
2023-04-27 11:12:51 +0200

    chicken can move like wizard

--- src/main/java/com/codecool/dungeoncrawl/data/actors/enemies/Chicken.java
+++ src/main/java/com/codecool/dungeoncrawl/data/actors/enemies/Chicken.java
@@ -3,13 +3,15 @@ package com.codecool.dungeoncrawl.data.actors.enemies;
 import com.codecool.dungeoncrawl.data.Cell;
 import com.codecool.dungeoncrawl.data.CellType;
 
-public class Chicken extends Enemy {
+public class Chicken extends Wizard {
 
     private int health = 200;
 
     private int attack = 3;
     private boolean moveUp = true;
+    private boolean moveLeft;
     private boolean moveDown;
+    private boolean moveRight;
 
 
     public Chicken(Cell cell) {
@@ -51,22 +53,42 @@ public class Chicken extends Enemy {
     @Override
     protected void moveFieldChecker(int position) {
 
-        if (!getCell().getNeighbor(0, position + 1).getType().equals(CellType.GRASS)) {
-            this.moveUp = false;
-            this.moveDown = true;
-        } else if (!getCell().getNeighbor(0, position - 1).getType().equals(CellType.GRASS)) {
-            this.moveUp = true;
-            this.moveDown = false;
+        if(!getCell().getNeighbor(0, position - 1).getType().equals(CellType.GRASS) &&
+                getCell().getNeighbor(position - 1, 0).getType().equals(CellType.GRASS)){
+            System.out.println("left");
+            moveUp = false;
+            moveLeft = true;
+        }else if (!getCell().getNeighbor(position - 1, 0).getType().equals(CellType.GRASS) &&
+                getCell().getNeighbor(0, position + 1).getType().equals(CellType.GRASS)){
+            System.out.println("down");
+            moveLeft = false;
+            moveDown = true;
+        }else if (!getCell().getNeighbor(0, position + 1).getType().equals(CellType.GRASS) &&
+                getCell().getNeighbor(position + 1, 0).getType().equals(CellType.GRASS)){
+            System.out.println("right");
+            moveDown = false;
+            moveRight = true;
+        }else if(!getCell().getNeighbor(position + 1, 0).getType().equals(CellType.GRASS) &&
+                getCell().getNeighbor(0, position - 1).getType().equals(CellType.GRASS)){
+            System.out.println("up");
+            moveRight = false;
+            moveUp = true;
         }
     }
 
     @Override
     protected void makeMove(int position) {
-        if (moveUp) {
+        if(moveRight){
             position++;
+            move(position,0);
+        }else if (moveLeft){
+            position--;
+            move(position, 0);
+        }else if(moveUp){
+            position--;
             move(0, position);
         } else if (moveDown) {
-            position--;
+            position++;
             move(0, position);
         }
     }
